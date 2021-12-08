7771372739d456ab55142cd71435142b76588764
Alvin Dizon <alvin.dizon91@gmail.com>
2021-12-08 10:46:37 +0800

    Part 2 setup nav, fix dark bottom nav by using material theme
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
