ef8a56654c9d3a1b6b1a83486df3646d65995e5f
Alvin Dizon <alvin.dizon91@gmail.com>
2021-12-30 20:50:13 +0800

    Modify NewsAdapter code and make it viewbinding and Pager friendly
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsAdapter.kt
@@ -0,0 +1,65 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.data.api.model.Article
+import com.androiddevs.mvvmnewsapp.databinding.ItemArticlePreviewBinding
+import com.bumptech.glide.Glide
+
+class NewsAdapter(val listener: (Article) -> Unit) :
+    PagingDataAdapter<Article, NewsAdapter.ArticleViewHolder>(differ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemArticlePreviewBinding.inflate(layoutInflater, parent, false)
+        return ArticleViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        getItem(position)?.let { item ->
+            holder.bind(item)
+        }
+    }
+
+    inner class ArticleViewHolder(private val binding: ItemArticlePreviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(article: Article) {
+            binding.apply {
+                Glide.with(binding.ivArticleImage).load(article.urlToImage).into(ivArticleImage)
+                tvSource.text = article.source.name
+                tvTitle.text = article.title
+                tvDescription.text = article.description
+                tvPublishedAt.text = article.publishedAt
+            }
+        }
+    }
+
+    companion object {
+        private val differ = object : DiffUtil.ItemCallback<Article>() {
+            override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return oldItem.url == newItem.url
+            }
+
+            override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
